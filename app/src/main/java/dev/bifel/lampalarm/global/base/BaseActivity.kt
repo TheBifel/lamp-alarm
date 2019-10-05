d28119dc693259dfb979c28f4ede42f155e8f69c
Bohdan Ishchenko <thebifel345@gmail.com>
2019-10-05 04:00:26 +0300

    added MainActivity

---
+++ app/src/main/java/dev/bifel/lampalarm/global/base/BaseActivity.kt
@@ -0,0 +1,12 @@
+package dev.bifel.lampalarm.global.base
+
+import android.app.Activity
+
+/**
+ * Date: 05.10.2019
+ * Time: 3:54
+ *
+ * @author Bohdan Ishchenko
+ */
+
+abstract class BaseActivity : Activity()
---
+++ app/src/main/java/dev/bifel/lampalarm/page/main/presentation/ui/MainActivity.kt
@@ -0,0 +1,18 @@
+package dev.bifel.lampalarm.page.main.presentation.ui
+
+import android.os.Bundle
+import dev.bifel.lampalarm.R
+import dev.bifel.lampalarm.global.base.BaseActivity
+
+/**
+ * Date: 05.10.2019
+ * Time: 3:52
+ *
+ * @author Bohdan Ishchenko
+ */
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
