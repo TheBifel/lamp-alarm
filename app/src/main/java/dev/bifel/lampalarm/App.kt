1e6e21fe5b5cd2611a86134f94d26f754fc1161e
Bohdan Ishchenko <thebifel345@gmail.com>
2019-10-05 03:10:32 +0300

    initial commit

---
+++ app/src/main/java/dev/bifel/lampalarm/App.kt
@@ -0,0 +1,16 @@
+package dev.bifel.lampalarm
+
+import android.app.Application
+
+/**
+ * Date: 05.10.2019
+ * Time: 2:58
+ *
+ * @author Bohdan Ishchenko
+ */
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
